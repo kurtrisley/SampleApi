225b8f5a493a30a5114bceebe7ad412f1dd873c9
Kurt Risley <kurtdrisley@gmail.com>
2020-04-16 14:12:29 -0600

    Add project files.

---
+++ SampleApi/Controllers/DevelopSystemTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Controllers
+{
+    public class DevelopSystemTime : ISystemTime
+    {
+        DateTime ISystemTime.GetCurrent()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ SampleApi/Controllers/StandardShippingEstimator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Controllers
+{
+    public class StandardShippingEstimator : IEstimateShipping
+    {
+        ISystemTime SystemTime;
+        DateTime IEstimateShipping.GetEstimatedShipDate()
+        {
+            return SystemTime.GetCurrent().Hour <= 17 ?
+                SystemTime.GetCurrent() :
+                SystemTime.GetCurrent().AddDays(1).Date;
+        }
+    }
+
+    public interface ISystemTime
+    {
+        DateTime GetCurrent();
+    }
+
+    public class SystemTime : ISystemTime
+    {
+        DateTime ISystemTime.GetCurrent()
+        {
+            return DateTime.Now;
+        }
+    }
+}
